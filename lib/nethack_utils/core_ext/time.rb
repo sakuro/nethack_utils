818bd03bb7a40af1c554b96bc2fe87822e9a217c
OZAWA Sakuro <sakuro@2238club.org>
2012-05-18 02:34:48 +0900

    Remove extensions to the Time class

--- lib/nethack_utils/core_ext/time.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding: US-ASCII -*-
-
-require 'nethack_utils/friday_13th'
-require 'nethack_utils/phase_of_the_moon'
-
-class Time
-  include NethackUtils::Friday13th
-  include NethackUtils::PhaseOfTheMoon
-end
